b703ef18183f44b81357b8c30eb480a7b660e0f1
RANKblank36 <tendosana@gmail.com>
2025-03-08 18:16:00 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "cherry", "mango"]
+print("List of fruits:", fruits)
+
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+fruits[1] = "blueberry"
+print("Updated list:", fruits)
+
+fruits.append("orange")
+fruits.insert(2, "grape")
+print("List after adding elements:", fruits)
